dd7b8003c58b25dccab3ef8ddfcc53997f0c5bae
Christos-Spyridon Karydis <115801950+chriskarydis@users.noreply.github.com>
2023-05-11 00:55:02 +0300

    Update models_gr.py

--- models_gr.py
+++ models_gr.py
@@ -4,7 +4,7 @@ Tower of Hanoi for Python - Models Module
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-# Color Constants class
+# Κλάση για σταθερές χρώματος
 class ColorConstants():
         RED = (255, 0, 0)
         BLACK = (0, 0, 0)
@@ -12,7 +12,7 @@ class ColorConstants():
         WHITE = (255, 255, 255)
         BACKGROUND = (240, 248, 255)
         BOARD_COLOR = (153, 76, 0)
-# Generic Block class
+# Κλάση γενικό μπλοκ
 class Block(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
@@ -23,19 +23,19 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
     def is_clicked(self):
         return pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos())
-# Game positions class
+# Κλάση Θέσεων παιχνιδιού
 class Position(Block):
     def __init__(self, pos_index,color, width, height):
         Block.__init__(self,color, width, height)
         self.pos_index = pos_index
         self.discs = []
-# Game discs class
+# Κλάση δίσκων παιχνιδιού
 class Disc(Block):
     def __init__(self,current_pos,id,color, width, height):
         Block.__init__(self,color, width, height)
         self.current_pos = current_pos
         self.id = id
-# Buttons class
+# Κλάση κουμπιών
 class Button(Block):
     def __init__(self,text,text_color,text_size,text_font,color, width, height):
         Block.__init__(self,color, width, height)
@@ -50,7 +50,7 @@ class Button(Block):
         w = (self.width/2-(self.text_render.get_width()/2))
         h = (self.height/2-(self.text_render.get_height()/2))
         self.image.blit(self.text_render,[w,h])
-# Main Menu
+# Κεντρικό μενού
 class MainMenu(ColorConstants):
     def __init__(self,SCREEN_WIDTH,SCREEN_HEIGHT):
         self.SCREEN_WIDTH = SCREEN_WIDTH
@@ -70,7 +70,7 @@ class MainMenu(ColorConstants):
             btn.set_value(i)
             self.btn_discs.append(btn)
         self.sprites_list.add(self.btn_discs)
-        # Game over buttons
+        # Game over κουμπιά
         self.btn_play_again = Button("Παίξε ξανά",self.BLACK,30,'Calibri',self.GREEN,130,30)
         self.btn_return = Button("Μενού",self.BLACK,30,'Calibri',self.BACKGROUND,150,30)
         self.btn_quit = Button("Έξοδος",self.BLACK,30,'Calibri',self.RED,90,30)
@@ -83,13 +83,13 @@ class MainMenu(ColorConstants):
         self.btn_quit.rect.x = self.SCREEN_WIDTH/2 + (self.btn_return.image.get_width())
         self.btn_quit.rect.y = self.SCREEN_HEIGHT/2 - 40
         self.btn_quit.render_text()
-# Game main class
+# Κύρια κλάση παιχνιδιού
 class Game(ColorConstants):
     def __init__(self,SCREEN_WIDTH,SCREEN_HEIGHT):
         # Game sprites groups
         self.sprites_list = pygame.sprite.Group()
         self.pos_sprites_list = pygame.sprite.Group()
-        # Game constants
+        # Σταθερές παιχνιδιού
         self.BOARD_WIDTH = SCREEN_WIDTH/2
         self.BOARD_HEIGHT = 50
         self.BOARD_X = SCREEN_WIDTH * 0.25
@@ -98,10 +98,10 @@ class Game(ColorConstants):
         self.POS_HEIGHT = 200
         self.DISC_WIDTH = 200
         self.DISC_HEIGHT = self.POS_WIDTH
-        # Positions and discs lists
+        # Θέσεις και λίστες δίσκων
         self.positions = []
         self.discs = []
-        # Draw the game board and it's positions
+        # Σχεδιασμός του πίνακα του παιχνιδιού και της θέσης του
         self.game_board = Block(self.BOARD_COLOR, self.BOARD_WIDTH,self.BOARD_HEIGHT)
         self.game_board.rect.x= self.BOARD_X
         self.game_board.rect.y= self.BOARD_Y
@@ -117,11 +117,11 @@ class Game(ColorConstants):
         self.positions = [first_pos,second_pos,third_pos]
         self.sprites_list.add([self.game_board,self.positions])
         self.pos_sprites_list.add(self.positions)
-    # Set discs number and mim movements
+    # Ορισμός του αριθμού των δίσκων και των ελάχιστων κινήσεων
     def set_n_discs(self,n_discs):
         self.n_discs = n_discs
         self.min_moves = ((2**self.n_discs)-1)
-    # Draw discs method
+    # Μέθοδος σχηματισμού δίσκων
     def draw_discs(self):
         DISC_WIDTH = 200
         DISC_HEIGHT = self.POS_WIDTH
